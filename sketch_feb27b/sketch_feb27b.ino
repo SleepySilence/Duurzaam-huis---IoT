ae116b4fd109fe0bf9b3f9906485e0e2b350d495
ahmad <37993@ma-web.nl>
2024-03-05 09:36:59 +0100

    eerste-lessen

---
+++ sketch_feb27b/sketch_feb27b.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(D3, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(D3, HIGH);
+  delay(1500);
+  digitalWrite(D3, LOW);
+  delay(250);
+  digitalWrite(D3, HIGH);
+  delay(500);
+  digitalWrite(D3, LOW);
+  delay(500);
+    digitalWrite(D3, HIGH);
+  delay(1500);
+  digitalWrite(D3, LOW);
+  delay(500);
+   digitalWrite(D3, HIGH);
+   delay(1500);
+  digitalWrite(D3, LOW);
+  delay(500);
+     digitalWrite(D3, HIGH);
+   delay(1500);
+  digitalWrite(D3, LOW);
+  delay(1500);
+}
